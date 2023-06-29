80d1092111df8f7850118c23e894530c3e422546
JannisRoh <119804936+JannisRoh@users.noreply.github.com>
2023-06-30 00:14:49 +0200

    Add files via upload

--- app/static/scripts.js
+++ app/static/scripts.js
@@ -1,8 +1,10 @@
+
+// Animation effect triggered when object is visible. Code from this tutorial: https://www.youtube.com/watch?v=T33NN_pPeNI
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
-    console.log(entry)
+    console.log(entry);
     if (entry.isIntersecting) {
-        entry.target.classList.add('visible');
+      entry.target.classList.add('visible');
     }
   });
 });
@@ -10,4 +12,5 @@ const observer = new IntersectionObserver((entries) => {
 const hiddenElements = document.querySelectorAll('.invisibleanimate');
 hiddenElements.forEach((el) => observer.observe(el));
 
+// Making images non-draggable. There are so many images used in the background, that their draggability becomes annoying.
 $('img').attr('draggable', false);
