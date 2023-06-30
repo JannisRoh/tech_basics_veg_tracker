094230aaf179e51ae1d084fdff5b72938d940a57
JannisRoh <119804936+JannisRoh@users.noreply.github.com>
2023-06-30 19:20:32 +0200

    Add files via upload

--- app/__init__.py
+++ app/__init__.py
@@ -5,8 +5,6 @@ from flask_bootstrap import Bootstrap
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
-#Maybe Delete
-import sqlalchemy as sa
 
 # Initialization of the app itself, the config and libraries used.
 app = Flask(__name__)
@@ -17,15 +15,9 @@ migrate = Migrate(app,db)
 
 from app import routes, models
 
-#@app.before_first_request
+@app.before_first_request
 def init_db():
-    engine = sa.create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
-    inspector = sa.inspect(engine)
-    if not inspector.has_table("input"):
-        with app.app_context():
-            db.drop_all()
-            db.create_all()
-            app.logger.info('Initialized the database!')
+    db.create_all
 
 if __name__ == '__main__':
     app.run()
